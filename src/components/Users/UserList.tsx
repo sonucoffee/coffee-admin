b20d114d8d3d89fa0b9264ed7986fe4148a181ce
sonucoffee <sonu@coffee.ai>
2025-08-08 00:04:13 +0530

    Make workspace table rows fully clickable

--- src/components/Users/UserList.tsx
+++ src/components/Users/UserList.tsx
@@ -240,7 +240,11 @@ const UserList: React.FC = () => {
                   {workspaceState.workspaces.map((edge: any) => {
                     const workspace = edge.node;
                     return (
-                      <tr key={workspace.id} className="hover:bg-gray-50">
+                      <tr 
+                        key={workspace.id} 
+                        className="hover:bg-gray-50 cursor-pointer transition-colors"
+                        onClick={() => handleWorkspaceSelect(workspace)}
+                      >
                         <td className="px-6 py-4 whitespace-nowrap">
                           <div className="flex items-center">
                             <Building2 className="w-5 h-5 text-gray-400 mr-3" />
@@ -255,14 +259,7 @@ const UserList: React.FC = () => {
                           </div>
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap text-right">
-                          <Button
-                            variant="primary"
-                            size="sm"
-                            icon={ChevronRight}
-                            onClick={() => handleWorkspaceSelect(workspace)}
-                          >
-                            Manage Users
-                          </Button>
+                          <ChevronRight className="w-5 h-5 text-gray-400" />
                         </td>
                       </tr>
                     );
