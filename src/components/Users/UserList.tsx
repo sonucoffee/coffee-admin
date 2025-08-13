496ee0b60729437db2c7eaada91da2028205d0a9
sonucoffee <sonu@coffee.ai>
2025-08-13 13:41:47 +0530

    Updated UserList.tsx

--- src/components/Users/UserList.tsx
+++ src/components/Users/UserList.tsx
@@ -278,7 +278,9 @@ const UserList: React.FC = () => {
                             <div className="text-sm text-gray-500">
                               {workspace.domain || 'No domain'}
                             </div>
-                            <ChevronRight className="w-5 h-5 text-gray-400 ml-3" />
+                          <div className="flex items-center justify-end">
+                            <ChevronRight className="w-4 h-4 text-gray-400" />
+                          </div>
                           </div>
                         </td>
                       </tr>
